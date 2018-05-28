e16e72f79f4cfb6b1fc540066db4f5bb39436af9
Sergey Shinkarenko <sergey.shinkarenko@oxagile.com>
2018-05-28 12:15:37 +0300

    Initial Commit

---
+++ SSFormCells/SSFormCells.h
@@ -0,0 +1,19 @@
+//
+//  SSFormCells.h
+//  SSFormCells
+//
+//  Created by Sergey Shinkarenko on 5/28/18.
+//  Copyright © 2018 Sergey Shinkarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSFormCells.
+FOUNDATION_EXPORT double SSFormCellsVersionNumber;
+
+//! Project version string for SSFormCells.
+FOUNDATION_EXPORT const unsigned char SSFormCellsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSFormCells/PublicHeader.h>
+
+
